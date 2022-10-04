35196842f6bcd33b848992777526759dff209a04
Alvin <alvin.tol@hotmail.com>
2022-10-04 13:08:31 -0600

    adds dotenv dependancy

--- spotSafetyCheck.js
+++ spotSafetyCheck.js
@@ -1,9 +1,16 @@
 const puppeteer = require('puppeteer');
-
+require('dotenv').config()
 const buildPdf = async () => {
+
+  const { URL, EMAIL, PASSWORD } = process.env
+
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
+
+  await page.goto(URL)
+
+  page.close()
 };
 
 buildPdf()
