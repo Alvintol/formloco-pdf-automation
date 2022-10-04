75e2aa376dbacf8469c156e17b7ca816e19ad4a5
Alvin <alvin.tol@hotmail.com>
2022-10-04 14:15:53 -0600

    adds login credentials

--- spotSafetyCheck.js
+++ spotSafetyCheck.js
@@ -6,9 +6,22 @@ const buildPdf = async () => {
 
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-
-
   await page.goto(URL)
+
+  await page.waitForNetworkIdle()
+
+  const isLoginPage = () => page.evaluate(() => {
+    const html = document.querySelectorAll('.mat-form-field-infix .ng-tns-c91-1');
+    const htmlTags = Array.from(html).map(tag => tag.innerHTML)
+    return htmlTags
+  });
+
+  console.log('LOGGING IN...');
+  await page.type('[formcontrolname=email]', EMAIL)
+  await page.type('[formcontrolname=password]', PASSWORD)
+  
+  await page.click('mat-icon .mat-icon .notranslate .icon-169px-action .material-icons .mat-icon-no-color .ng-star-inserted')
+
 
   page.close()
 };
