a595215eb49296efef8185b4cdf91ae6d8aa346a
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-10-24 10:15:24 -0700

    update on details page -> finish automation up until check heater/ defroster

--- vehicleInspection.js
+++ vehicleInspection.js
@@ -67,6 +67,7 @@ require("dotenv").config();
 
   //Details section 
 
+  //Ignition Key sequence
   await clickHandler('//*[@id="mat-checkbox-1"]/label/span[1]')
   await page.type("[formcontrolname=comment]", "**TEST**")
   await clickHandler('//*[@id="mat-dialog-0"]/app-comment/div[2]/form/div[2]/div/button')
@@ -76,10 +77,104 @@ require("dotenv").config();
   await page.click('[formcontrolname=CorrectiveActionRequired]')
   await page.type('[formcontrolname=CorrectiveActionRequired]', '**TEST**')
   await clickHandler('//*[@id="mat-dialog-1"]/app-corrective-action/form/div[3]/mat-icon')
-  
+  await page.waitForXPath(' //*[@id="mat-dialog-0"]/app-comment/div[2]/div/button[3]')
+  await clickHandler('//*[@id="mat-dialog-0"]/app-comment/div[2]/div/button[3]')
+
+  //Oil Level sequence
+  await clickHandler('//*[@id="mat-checkbox-2"]/label/span[1]')
+  await page.type("[formcontrolname=comment]", '**TEST**');
+  await clickHandler('//*[@id="mat-dialog-2"]/app-comment/div[2]/form/div[2]/div')
+  await page.click('[formcontrolname=DateCorrectiveActionToBeCompleted]')
+  await page.waitForXPath('//*[@id="ion-overlay-2"]/div[2]/div[1]/div[2]')
+  await clickHandler('//*[@id="ion-overlay-2"]/div[2]/div[1]/div[2]')
+  await page.click('[formcontrolname=CorrectiveActionRequired]')
+  await page.type('[formcontrolname=CorrectiveActionRequired]', '**TEST**')
+  await clickHandler('//*[@id="mat-dialog-3"]/app-corrective-action/form/div[3]/mat-icon')
+  await page.waitForXPath('//*[@id="mat-dialog-2"]/app-comment/div[2]/div/button[3]')
+  await clickHandler('//*[@id="mat-dialog-2"]/app-comment/div[2]/div/button[3]')
+
+  //Coolant level sequence
+  await clickHandler('//*[@id="mat-checkbox-3"]/label/span[1]')
+  await page.type("[formcontrolname=comment]", '**TEST**');
+  await clickHandler('//*[@id="mat-dialog-4"]/app-comment/div[2]/form/div[2]/div')
+  await page.click('[formcontrolname=DateCorrectiveActionToBeCompleted]')
+  await page.waitForXPath('//*[@id="ion-overlay-3"]/div[2]/div[1]/div[2]')
+  await clickHandler('//*[@id="ion-overlay-3"]/div[2]/div[1]/div[2]')
+  await page.click('[formcontrolname=CorrectiveActionRequired]')
+  await page.type('[formcontrolname=CorrectiveActionRequired]', '**TEST**')
+  await clickHandler('//*[@id="mat-dialog-5"]/app-corrective-action/form/div[3]/mat-icon')
+  await page.waitForXPath('//*[@id="mat-dialog-4"]/app-comment/div[2]/div/button[3]')
+  await clickHandler('//*[@id="mat-dialog-4"]/app-comment/div[2]/div/button[3]')
+
+  //Washer Fluid Level sequence 
+  await clickHandler('//*[@id="mat-checkbox-4"]/label/span[1]')
+  await page.type("[formcontrolname=comment]", '**TEST**');
+  await clickHandler('//*[@id="mat-dialog-6"]/app-comment/div[2]/form/div[2]/div')
+  await page.click('[formcontrolname=DateCorrectiveActionToBeCompleted]')
+  await page.waitForXPath('//*[@id="ion-overlay-4"]/div[2]/div[1]/div[2]')
+  await clickHandler('//*[@id="ion-overlay-4"]/div[2]/div[1]/div[2]')
+  await page.click('[formcontrolname=CorrectiveActionRequired]')
+  await page.type('[formcontrolname=CorrectiveActionRequired]', '**TEST**')
+  await clickHandler('//*[@id="mat-dialog-7"]/app-corrective-action/form/div[3]/mat-icon')
+  await page.waitForXPath('//*[@id="mat-dialog-6"]/app-comment/div[2]/div/button[3]')
+  await clickHandler('//*[@id="mat-dialog-6"]/app-comment/div[2]/div/button[3]')
+
+  // Power Steering Fluid Level sequence
+  await clickHandler('//*[@id="mat-checkbox-5"]/label/span[1]')
+  await page.type("[formcontrolname=comment]", '**TEST**');
+  await clickHandler('//*[@id="mat-dialog-8"]/app-comment/div[2]/form/div[2]/div')
+  await page.click('[formcontrolname=DateCorrectiveActionToBeCompleted]')
+  await page.waitForXPath('//*[@id="ion-overlay-5"]/div[2]/div[1]/div[2]')
+  await clickHandler('//*[@id="ion-overlay-5"]/div[2]/div[1]/div[2]')
+  await page.click('[formcontrolname=CorrectiveActionRequired]')
+  await page.type('[formcontrolname=CorrectiveActionRequired]', '**TEST**')
+  await clickHandler('//*[@id="mat-dialog-9"]/app-corrective-action/form/div[3]/mat-icon')
+  await page.waitForXPath('//*[@id="mat-dialog-8"]/app-comment/div[2]/div/button[3]')
+  await clickHandler('//*[@id="mat-dialog-8"]/app-comment/div[2]/div/button[3]')
+
+  //Check for Air Gauge sequence 
+  await clickHandler('//*[@id="mat-checkbox-6"]/label/span[1]')
+  await page.type("[formcontrolname=comment]", '**TEST**');
+  await clickHandler('//*[@id="mat-dialog-10"]/app-comment/div[2]/form/div[2]/div')
+  await page.click('[formcontrolname=DateCorrectiveActionToBeCompleted]')
+  await page.waitForXPath('//*[@id="ion-overlay-6"]/div[2]/div[1]/div[2]')
+  await clickHandler('//*[@id="ion-overlay-6"]/div[2]/div[1]/div[2]')
+  await page.click('[formcontrolname=CorrectiveActionRequired]')
+  await page.type('[formcontrolname=CorrectiveActionRequired]', '**TEST**')
+  await clickHandler('//*[@id="mat-dialog-11"]/app-corrective-action/form/div[3]/mat-icon')
+  await page.waitForXPath('//*[@id="mat-dialog-10"]/app-comment/div[2]/div/button[3]')
+  await clickHandler('//*[@id="mat-dialog-10"]/app-comment/div[2]/div/button[3]')
+
+  //Check Horn sequence 
+  await clickHandler('//*[@id="mat-checkbox-7"]/label/span[1]')
+  await page.type("[formcontrolname=comment]", '**TEST**');
+  await clickHandler('//*[@id="mat-dialog-12"]/app-comment/div[2]/form/div[2]/div')
+  await page.click('[formcontrolname=DateCorrectiveActionToBeCompleted]')
+  await page.waitForXPath('//*[@id="ion-overlay-7"]/div[2]/div[1]/div[2]')
+  await clickHandler('//*[@id="ion-overlay-7"]/div[2]/div[1]/div[2]')
+  await page.click('[formcontrolname=CorrectiveActionRequired]')
+  await page.type('[formcontrolname=CorrectiveActionRequired]', '**TEST**')
+  await clickHandler('//*[@id="mat-dialog-13"]/app-corrective-action/form/div[3]/mat-icon')
+  await page.waitForXPath('//*[@id="mat-dialog-12"]/app-comment/div[2]/div/button[3]')
+  await clickHandler('//*[@id="mat-dialog-12"]/app-comment/div[2]/div/button[3]')
+
+  //Check Heater/Defroster sequence 
+  await clickHandler('//*[@id="mat-checkbox-8"]/label/span[1]')
+  await page.type("[formcontrolname=comment]", '**TEST**');
+  await clickHandler('//*[@id="mat-dialog-14"]/app-comment/div[2]/form/div[2]/div')
+  await page.click('[formcontrolname=DateCorrectiveActionToBeCompleted]')
+  await page.waitForXPath('//*[@id="ion-overlay-8"]/div[2]/div[1]/div[2]')
+  await clickHandler('//*[@id="ion-overlay-8"]/div[2]/div[1]/div[2]')
+  await page.click('[formcontrolname=CorrectiveActionRequired]')
+  await page.type('[formcontrolname=CorrectiveActionRequired]', '**TEST**')
+  await clickHandler('//*[@id="mat-dialog-15"]/app-corrective-action/form/div[3]/mat-icon')
+  await page.waitForXPath('//*[@id="mat-dialog-14"]/app-comment/div[2]/div/button[3]')
+  await clickHandler('//*[@id="mat-dialog-14"]/app-comment/div[2]/div/button[3]')
 
 
-  // await clickHandler('//*[@id="mat-checkbox-2"]/label/span[1]')
-  // await clickHandler('//*[@id="mat-checkbox-3"]/label/span[1]')
+
+
+
+
 
 })();
